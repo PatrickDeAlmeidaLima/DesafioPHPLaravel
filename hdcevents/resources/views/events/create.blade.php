c72b380cde609f2ee6729138a09987a1df18915f
unknown <patrick.almeida.lima@gmail.com>
2023-03-05 18:09:11 -0300

    Criando rotas para imagem e migration

--- hdcevents/resources/views/events/create.blade.php
+++ hdcevents/resources/views/events/create.blade.php
@@ -3,30 +3,36 @@
 @section('title', 'Criar Evento')
 
 @section('content')
+
     <div id="event-create-container" class="col-md-6 offset-md-3">
-        <h1>Crie seu evento</h1>
-        <form action="/events" method="POST">
+        <h1>Crie o seu evento</h1>
+        <form action="/events" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="form-group">
-                <label for="title">Evento: </label>
+                <label for="image">Imagem do evento:</label>
+                <input type="file" id="image" class="form-control-file">
+            </div>
+            <div class="form-group">
+                <label for="title">Evento:</label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
             </div>
             <div class="form-group">
-                <label for="title">Cidade: </label>
+                <label for="title">Cidade:</label>
                 <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
             </div>
             <div class="form-group">
-                <label for="title">O evento é privado? </label>
+                <label for="title">O evento é privado?</label>
                 <select name="private" id="private" class="form-control">
                     <option value="0">Não</option>
                     <option value="1">Sim</option>
-                  </select>
+                </select>
             </div>
             <div class="form-group">
-                <label for="title">Descrição: </label>
+                <label for="title">Descrição:</label>
                 <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
             </div>
-            <input type="submit" class="btn btn-primary" value="Criar evento">
+            <input type="submit" class="btn btn-primary" value="Criar Evento">
         </form>
     </div>
+
 @endsection
